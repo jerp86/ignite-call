7778f57022cd0ee09bfb4ae5de0009085d6c77cf
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-02-19 17:47:17 -0300

    feat (api): add schema validation, save in db and update return

--- src/pages/api/users/time-intervals.api.ts
+++ src/pages/api/users/time-intervals.api.ts
@@ -1,6 +1,36 @@
+import { prisma } from '@/lib/prisma'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getServerSession } from 'next-auth/next'
+import { z } from 'zod'
 import { buildNextAuthOptions } from '../auth/[...nextauth].api'
+
+const timeIntervalsBodySchema = z.object({
+  intervals: z
+    .array(
+      z.object({
+        weekDay: z.number(),
+        startTimeInMinutes: z.number(),
+        endTimeInMinutes: z.number(),
+      }),
+    )
+    .min(1, {
+      message: 'Você precisa selecionar pelo menos um dia da semana!',
+    })
+    .max(7, {
+      message: 'Você precisa selecionar no máximo sete dias da semana!',
+    })
+    .refine(
+      (intervals) =>
+        intervals.every(
+          (interval) =>
+            interval.endTimeInMinutes - 60 >= interval.startTimeInMinutes,
+        ),
+      {
+        message:
+          'O horário de término deve ser pelo menos 1h distante do início',
+      },
+    ),
+})
 
 export default async function handler(
   req: NextApiRequest,
@@ -16,9 +46,25 @@ export default async function handler(
     buildNextAuthOptions(req, res),
   )
 
-  if (!session) {
+  if (!session || !session.user.id) {
     return res.status(401).end()
   }
 
-  return res.status(201).json({ session })
+  const { intervals } = timeIntervalsBodySchema.parse(req.body)
+
+  // SQLite não aceita o createMany - await prisma.userTimeInterval.createMany
+  await Promise.all(
+    intervals.map((interval) => {
+      return prisma.userTimeInterval.create({
+        data: {
+          week_day: interval.weekDay,
+          time_start_in_minutes: interval.startTimeInMinutes,
+          time_end_in_minutes: interval.endTimeInMinutes,
+          user_id: session.user?.id,
+        },
+      })
+    }),
+  )
+
+  return res.status(201).end()
 }
