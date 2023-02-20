72d7f918818ba979113347c3581b66ecd2cf8f76
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-02-19 21:00:02 -0300

    chore (config): add images domain

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     appDir: true,
   },
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  images: {
+    domains: ['https://lh3.googleusercontent.com/'],
+  },
 }
 
 module.exports = nextConfig
